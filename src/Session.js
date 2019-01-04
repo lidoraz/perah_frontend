baa9a33af769cfea81082e6a78aa26a5c947b241
lidoraz <lidor.az@gmail.com>
2019-01-05 01:59:52 +0200

    mo

--- src/Session.js
+++ src/Session.js
@@ -233,6 +233,7 @@ export default class Session extends React.Component {
               <img
                 name="currImage"
                 src={this.state.currImageSrc}
+                style={width: 40%}
                 // style={{ width: 100, height: 100 }}
               />
             )}
