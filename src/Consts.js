36b20f0c3b7171d75637fc346058fc25c90b77a7
lidoraz <lidor.az@gmail.com>
2019-02-27 21:14:23 +0200

    a

--- src/Consts.js
+++ src/Consts.js
@@ -1,8 +1,8 @@
 const GLOBAL_VARS = {
   backendIP: "http://35.176.94.224:8080/",
   // backendIP: "http://localhost:8080/",
-  timeLimit: 4000,
-  isTimeLimited: false,
+  timeLimit: 5000,
+  isTimeLimited: true,
 
   numbers: "1234567890",
   letters: "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWQYZ`'-",
